2a43fd7d93c4904d08c21f8ab5a6664c2409fe7d
Rafael201007 <87946342+Rafael201007@users.noreply.github.com>
2025-03-12 02:14:21 -0300

    fixing some logic in object class

--- game/pygameClasses.py
+++ game/pygameClasses.py
@@ -10,9 +10,12 @@ class Object():
     def __init__(self, image, pos):
         self.Surface = pygame.image.load(image).convert_alpha()
         self.Rect = self.Surface.get_rect(topleft=pos)
+        self.Position = pos
     def UpdateRectPos(self, pos):
         self.Rect = self.Surface.get_rect(topleft=pos)
+        self.Position = pos
     def Render(self, screenR):
+        self.UpdateRectPos(self.Position)
         screenR.blit(self.Surface, self.Rect)
     def Scale(self, xy):
         self.Surface = pygame.transform.scale(self.Surface, xy)
